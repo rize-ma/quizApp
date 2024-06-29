b36c876c44f692cd316df0b4628ff37ed7eccbd4
rize-ma <masaki030603@gmail.com>
2024-06-30 05:29:56 +0900

    定義したテーブル情報をもとにmodelを作成

--- api/src/models.rs
+++ api/src/models.rs
@@ -1,19 +1,46 @@
-use crate::schema::posts;
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = posts)]
+#[derive(Queryable, Identifiable, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::users)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct Post {
+pub struct User {
     pub id: i32,
-    pub title: String,
-    pub body: String,
-    pub published: bool,
+    pub username: String,
+    pub icon_url: Option<String>,
+    pub correct_answers_count: i32,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
 }
 
-#[derive(Insertable)]
-#[diesel(table_name = posts)]
-pub struct NewPost<'a> {
-    pub title: &'a str,
-    pub body: &'a str,
+#[derive(Queryable, Identifiable, Associations, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::quizzes)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[belongs_to(User, foreign_key = "created_by")]
+pub struct Quiz {
+    pub id: i32,
+    pub question: String,
+    pub option1: String,
+    pub option2: String,
+    pub option3: String,
+    pub option4: String,
+    pub correct_option: i32,
+    pub created_by: i32,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
+
+#[derive(Queryable, Identifiable, Associations, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::user_quiz_results)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[belongs_to(User)]
+#[belongs_to(Quiz)]
+pub struct UserQuizResult {
+    pub id: i32,
+    pub user_id: i32,
+    pub quiz_id: i32,
+    pub selected_option: i32,
+    pub is_correct: bool,
+    pub answered_at: NaiveDateTime,
 }
--- api/src/schema.rs
+++ api/src/schema.rs
@@ -1,11 +1,13 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    posts (id) {
+    users (id) {
         id -> Int4,
-        title -> Varchar,
-        body -> Text,
-        published -> Bool,
+        username -> Varchar,
+        icon_url -> Nullable<Varchar>,
+        correct_answers_count -> Int4,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
     }
 }
 
@@ -18,33 +20,20 @@ diesel::table! {
         option3 -> Text,
         option4 -> Text,
         correct_option -> Int4,
-        created_by -> Nullable<Int4>,
-        created_at -> Nullable<Timestamp>,
-        updated_at -> Nullable<Timestamp>,
+        created_by -> Int4,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
     }
 }
 
 diesel::table! {
     user_quiz_results (id) {
         id -> Int4,
-        user_id -> Nullable<Int4>,
-        quiz_id -> Nullable<Int4>,
+        user_id -> Int4,
+        quiz_id -> Int4,
         selected_option -> Int4,
         is_correct -> Bool,
-        answered_at -> Nullable<Timestamp>,
-    }
-}
-
-diesel::table! {
-    users (id) {
-        id -> Int4,
-        #[max_length = 255]
-        username -> Varchar,
-        #[max_length = 255]
-        icon_url -> Nullable<Varchar>,
-        correct_answers_count -> Nullable<Int4>,
-        created_at -> Nullable<Timestamp>,
-        updated_at -> Nullable<Timestamp>,
+        answered_at -> Timestamp,
     }
 }
 
@@ -52,9 +41,4 @@ diesel::joinable!(quizzes -> users (created_by));
 diesel::joinable!(user_quiz_results -> quizzes (quiz_id));
 diesel::joinable!(user_quiz_results -> users (user_id));
 
-diesel::allow_tables_to_appear_in_same_query!(
-    posts,
-    quizzes,
-    user_quiz_results,
-    users,
-);
+diesel::allow_tables_to_appear_in_same_query!(quizzes, user_quiz_results, users,);
