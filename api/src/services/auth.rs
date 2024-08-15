8f20aa3d2b4dbb0bdc3b3fc9421dea3354589042
rize-ma <masaki030603@gmail.com>
2024-08-16 07:54:50 +0900

    ログインのロジックを追加

--- api/src/services/auth.rs
+++ api/src/services/auth.rs
@@ -5,7 +5,7 @@ use crate::{
     schema::users::dsl::{email, user_id, users},
     utils::auth::{find_user, hash_password},
 };
-use actix_web::{web, Error, HttpResponse};
+use actix_web::{web, HttpResponse};
 use serde_json::json;
 
 use diesel::prelude::*;
@@ -13,8 +13,23 @@ use diesel::prelude::*;
 pub struct AuthService;
 
 impl AuthService {
-    pub fn login(pool: web::Data<DbPool>, form_data: LoginUser) -> Result<User, Error> {
+    pub fn login(pool: web::Data<DbPool>, form_data: LoginUser) -> Result<HttpResponse, AuthError> {
         let mut conn = pool.get().unwrap();
+
+        let user = find_user(&mut conn, Box::new(email.eq(form_data.email.clone())))?
+            .ok_or(AuthError::EmailNotFound)?;
+
+        let hashed_password = hash_password(&form_data.password.as_bytes())?;
+        if user.password != hashed_password {
+            return Err(AuthError::IncorrectPassword);
+        };
+
+        let token = create_token(user.id);
+
+        Ok(HttpResponse::Ok().json(json!({
+            "user": user,
+            "token": token
+        })))
     }
 
     pub fn signup(
@@ -22,14 +37,15 @@ impl AuthService {
         form_data: SignupUser,
     ) -> Result<HttpResponse, AuthError> {
         let mut conn = pool.get().unwrap();
-        if let Some(_) = find_user(&mut conn, Box::new(email.eq(&form_data.email)))? {
+
+        if find_user(&mut conn, Box::new(email.eq(form_data.email.clone())))?.is_some() {
             return Err(AuthError::EmailAlreadyRegistered);
         }
-        if let Some(_) = find_user(&mut conn, Box::new(user_id.eq(&form_data.user_id)))? {
+        if find_user(&mut conn, Box::new(user_id.eq(form_data.user_id.clone())))?.is_some() {
             return Err(AuthError::UserIdAlreadyRegistered);
         }
-        let hashed_password = hash_password(&form_data.password.as_bytes())?;
 
+        let hashed_password = hash_password(&form_data.password.as_bytes())?;
         let new_user = SignupUser {
             email: form_data.email,
             password: hashed_password,
