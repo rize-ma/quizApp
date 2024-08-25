8625422cbc9c869b6e6c55ce04830c28a9bcd3cb
rize-ma <masaki030603@gmail.com>
2024-08-25 12:59:29 +0900

    テーブル更新時にUpdatedAtに自動で更新時間が設定されるようにする

---
+++ api/migrations/2024-08-25-032654_create_user/up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    correct_answers_count INT DEFAULT 0,
+    email TEXT NOT NULL,
+    icon_url VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
+    self_introduction TEXT,
+    user_id VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER trigger_update_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
