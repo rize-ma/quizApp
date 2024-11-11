9af71dde9236ae18adf102e0fb4879725b72b9e5
rize-ma <masaki030603@gmail.com>
2024-11-12 00:36:26 +0900

    ログイン後の画面遷移を実装

--- front/src/api/axiosInstance.ts
+++ front/src/api/axiosInstance.ts
@@ -3,3 +3,16 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
