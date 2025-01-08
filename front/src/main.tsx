9eb227cd94b5b18922716be59de6de15f94c8d2c
rize-ma <masaki030603@gmail.com>
2025-01-08 15:26:02 +0900

    不要なimport削除

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Auth } from './page/Auth';
 import { HelmetProvider } from 'react-helmet-async';
 import PrivateRoute from './PrivateRoute';
