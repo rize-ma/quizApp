f81280e912bb1fa2c91af7911f52f4191fbae08d
rize-ma <masaki030603@gmail.com>
2025-01-18 22:30:44 +0900

    クイズに挑戦ボタンのクリックイベントが正常に発火するようにする

--- front/src/page/QuizStart.tsx
+++ front/src/page/QuizStart.tsx
@@ -1,18 +1,21 @@
 import { Button } from '@/components/ui/button/button';
 import { FC } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useNavigate } from 'react-router-dom';
 
 export const QuizStart: FC = () => {
+  const navigate = useNavigate();
+  const onClickStart = () => {
+    navigate('/quiz/play');
+  };
   return (
     <>
       <Helmet>
         <title>クイズに挑戦</title>
       </Helmet>
       <div className="w-full flex justify-center items-center">
-        <Button className="w-52">
-          <a href="/quiz/play">
-            <span>クイズに挑戦する</span>
-          </a>
+        <Button className="w-52" onClick={onClickStart}>
+          クイズに挑戦する
         </Button>
       </div>
     </>
